64118d6f9616fbddcccad07e20cb0ad16e43dc50
SteveSu <steve.su@langlive.com>
2022-02-20 20:02:44 +0800

    LC617::mergeTrees

--- tree.h
+++ tree.h
@@ -34,6 +34,7 @@ struct TreeNode {
 class tree {};
 
 /***********  Easy  **********/
+struct LC617 { TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2); };
 struct LC606 { string tree2str(TreeNode* root); };
 struct LC543 { int diameterOfBinaryTree(TreeNode* root); };
 struct LC404 { int sumOfLeftLeaves(TreeNode* root); };
