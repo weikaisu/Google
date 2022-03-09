42c35de5871c38ab0fc5e1da83de58c065ed4d2f
SteveSu <steve.su@langlive.com>
2022-03-09 10:33:47 +0800

    LC0997::findJudge

--- list.h
+++ list.h
@@ -121,6 +121,7 @@ struct MyHashSet {
 private:
     array<forward_list<int>, 1000> t;
 };
+struct LC0997 { int findJudge(int n, vector<vector<int>>& trust); };
 struct LC0961 { int repeatedNTimes(vector<int>& nums); };
 struct LC0914 { bool hasGroupsSizeX(vector<int>& deck); };
 struct LC0888 { vector<int> fairCandySwap(vector<int>& aliceSizes, vector<int>& bobSizes); };
