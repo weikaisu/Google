7b874dae85b0c581a48bd5247c26a664e7938adc
SteveSu <steve.su@langlive.com>
2022-03-23 11:23:54 +0800

    LC0796::rotateString

--- string.cpp
+++ string.cpp
@@ -148,6 +148,21 @@ string LC0819::mostCommonWord(string paragraph, vector<string>& banned) {
     return res;
 }
 
+bool LC0796::rotateString(string s, string goal) {
+    // 一行完成的方法，就是我们其实可以在A之后再加上一个A，这样如果新的字符串(A+A)中包含B的话，说明A一定能通过偏移得到B。
+    // 就比如题目中的例子，A="abcde", B="bcdea"，那么A+A="abcdeabcde"，里面是包括B的，所以返回true即可
+    return s.size()==goal.size() && (s+s).find(goal) != string::npos;
+
+    // 这道题给了我们两个字符串A和B，定义了一种偏移操作，以某一个位置将字符串A分为两截，并将两段调换位置，如果此时跟字符串B相等了，
+    // 就说明字符串A可以通过偏移得到B。现在就是让我们判断是否存在这种偏移，那么最简单最暴力的方法就是遍历所有能将A分为两截的位置，
+    // 然后用取子串的方法将A断开，交换顺序，再去跟B比较，如果相等，返回true即可，遍历结束后，返回false
+//    if(s.size()!=goal.size()) return false;
+//    int n = s.size();
+//    for(int i=0; i<n; i++)
+//        if(s.substr(i,n-i)+s.substr(0,i) == goal) return true;
+//    return false;
+}
+
 string LC0709::toLowerCase(string s) {
     // 小写字母比其对应的大写字母的ASCII码大32，所以我们只需要遍历字符串，对于所有的大写字母，统统加上32即可
     for(auto &c:s)
--- string.h
+++ string.h
@@ -12,6 +12,7 @@ struct LC0953 { bool isAlienSorted(vector<string>& words, string order); };
 struct LC0929 { int numUniqueEmails(vector<string>& emails) ; };
 struct LC0884 { vector<string> uncommonFromSentences(string s1, string s2); };
 struct LC0819 { string mostCommonWord(string paragraph, vector<string>& banned); };
+struct LC0796 { bool rotateString(string s, string goal); };
 struct LC0709 { string toLowerCase(string s); };
 struct LC0696 { int countBinarySubstrings(string s) ; };
 struct LC0521 { int findLUSlength(string a, string b); };
