7d7d657a68eb10a7880f7a79a98d62f074f0e034
SteveSu <steve.su@langlive.com>
2022-06-04 16:50:21 +0800

    LC0303::sumRange

--- dp.cpp
+++ dp.cpp
@@ -1,6 +1,19 @@
 
 #include "dp.h"
 
+LC0303::LC0303(vector<int>& nums) {
+    // 这道题让我们检索一个数组的某个区间的所有数字之和，题目中给了两条条件，首先数组内容不会变化，其次有很多的区间和检索。那么我们用传统的遍
+    // 历相加来求每次区间和检索，十分的不高效，而且无法通过 OJ。所以这道题的难点就在于是否能想到来用建立累计直方图的思想来建立一个累计和的
+    // 数组dp，並通过增加一位 dp 的长度，来避免在 sumRange 中检测i是否为0
+    dp.push_back(0);
+    for(int i=1; i<=nums.size(); i++)
+        dp.push_back(dp[i-1]+nums[i-1]);
+}
+
+int LC0303::sumRange(int left, int right) {
+    return dp[right+1]-dp[left];
+}
+
 //LC0070 run;
 //cout << run.climbStairs(45) << endl;
 int LC0070::climbStairs(int n) {
--- dp.h
+++ dp.h
@@ -5,6 +5,8 @@
 #include "main.h"
 using namespace std;
 
+/*回傳陣列某一區間值之和*/
+struct LC0303 { vector<int> dp; LC0303(vector<int>& nums); int sumRange(int left, int right); };
 struct LC0070 { int climbStairs(int n); };
 struct LC0053 { int maxSubArray(vector<int>& nums); };
 
