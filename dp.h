1cdaabeb1fbee7ea5e787548ed455b7bd0b46b81
SteveSu <steve.su@langlive.com>
2022-06-04 18:56:30 +0800

    LC0455::findContentChildren

--- dp.h
+++ dp.h
@@ -5,10 +5,14 @@
 #include "main.h"
 using namespace std;
 
+// Dynamic Programming
 /*回傳陣列某一區間值之和*/
 struct LC0303 { vector<int> dp; LC0303(vector<int>& nums); int sumRange(int left, int right); };
 struct LC0070 { int climbStairs(int n); };
 struct LC0053 { int maxSubArray(vector<int>& nums); };
 
+//Greedy
+/*分發糖果給小孩*/
+struct LC0455 { int findContentChildren(vector<int>& g, vector<int>& s); };
 
 #endif //GOOGLE_DP_H
