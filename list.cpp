9dd55a663e833c0861f3d2759f796f56707e58ec
SteveSu <steve.su@langlive.com>
2022-06-04 17:07:22 +0800

    LC0414::thirdMax

--- list.cpp
+++ list.cpp
@@ -875,6 +875,28 @@ string LC0541::reverseStr(string s, int k) {
     return s;
 }
 
+int LC0414::thirdMax(vector<int>& nums) {
+    // 这道题让我们求数组中第三大的数，如果不存在的话那么就返回最大的数，题目中说明了这里的第三大不能和第二大相同，必须是严格的小于，而并非小
+    // 于等于。这道题并不是很难，如果知道怎么求第二大的数，那么求第三大的数的思路都是一样的。那么我们用三个变量first, second, third来分别
+    // 保存第一大，第二大，和第三大的数，然后我们遍历数组，如果遍历到的数字大于当前第一大的数first，那么三个变量各自错位赋值，如果当前数字大
+    // 于second，小于first，那么就更新second和third，如果当前数字大于third，小于second，那就只更新third，注意这里有个坑，就是初始化要
+    // 用长整型long的最小值，否则当数组中有INT_MIN存在时，程序就不知道该返回INT_MIN还是最大值first了
+    long first=LONG_MIN, second=LONG_MIN, third=LONG_MIN;
+    for(auto &n:nums) {
+        if(n > first) {
+            third=second;
+            second=first;
+            first=n;
+        } else if(n > second && n < first) {
+            third=second;
+            second=n;
+        } else if(n > third && n < second) {
+            third=n;
+        }
+     }
+    return (third==LONG_MIN || third==second) ? first : third;
+}
+
 void LC0344::reverseString(vector<char>& s) {
     int l=0, r=s.size()-1;
     while(l<r)
--- list.h
+++ list.h
@@ -225,6 +225,8 @@ struct LC0557 { string reverseWords(string s); };
 struct LC0551 { bool checkRecord(string s); };
 /*每2k個字符反轉k個字符*/
 struct LC0541 { string reverseStr(string s, int k); };
+/*回傳陣列裡第三大的數*/
+struct LC0414 { int thirdMax(vector<int>& nums); };
 /*反轉字符陣列*/
 struct LC0344 { void reverseString(vector<char>& s); };
 /*將陣列裡所有0移到後面*/
