1b2ee28f3c44f69f9055cefccb514fc70e7f7c9e
Steve Su <steve.su@amd.com>
2023-05-24 10:18:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main() {
     cout << "Hello, World!" << endl;
-
 //    vector<int> v{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 //    for(auto i:v) cout << i << ' ';
 //    cout << endl;
@@ -36,8 +35,6 @@ int main() {
 //    string s {"ABCDE"};
 //    for(auto c:s) cout << c << ' ';
 //    cout << endl;
-
-
 
     return 0;
 }
