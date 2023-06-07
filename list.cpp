016144f418aea7452fce7284c3a758426e93ce76
Steve Su <steve.su@amd.com>
2023-06-07 12:49:26 +0800

    LC0896::isMonotonic

--- list.cpp
+++ list.cpp
@@ -771,6 +771,30 @@ int LC0944::minDeletionSize(vector<string>& strs) {
     return res;
 }
 
+bool LC0896::isMonotonic(vector<int>& nums) {
+    // 判断一个数组是否单调，单调数组就是说这个数组的数字要么是递增的，要么是递减的，不存在一会儿递增一会儿递减的情况，即不会有山峰存在。
+    // 这里不是严格的递增或递减，是允许有相同的数字的。那么我们直接将相邻的两个数字比较一下即可，使用两个标识符，inc 和 dec，初始化均为
+    // true，我们开始时假设这个数组既是递增的又是递减的，当然这是不可能的，我们会在后面对其进行更新。在遍历数组的时候，只要发现某个数字大
+    // 于其身后的数字了，那么 inc 就会赋值为 false，同理，只要某个数字小于其身后的数字了，dec 就会被赋值为 false，所以在既有递增又有递
+    // 减的数组中，inc 和 dec 都会变为 false，而在单调数组中二者之间至少有一个还会保持为 true
+    bool inc=true, dec=true;
+    for(int i=1; i<nums.size(); i++) {
+        inc &= nums[i]>=nums[i-1];
+        dec &= nums[i]<=nums[i-1];
+        if(!inc && !dec) return false;
+    }
+    return true;
+
+    // 没有用 bool 型的，而是用了整型数字来记录递增和递减的个数，若是单调数组，那么最终在 inc 和 dec 中一定会有一个值是等于数组长度的
+//    int inc=1, dec=1; // 初始值是1，最後才會是跟n做比較
+//    int n=nums.size();
+//    for(int i=1; i<n; i++) {
+//        inc += nums[i]>=nums[i-1];
+//        dec += nums[i]<=nums[i-1];
+//    }
+//    return (inc==n || dec==n);
+}
+
 int LC0892::surfaceArea(vector<vector<int>>& grid) {
     // 给了我们一个二维数组 grid，其中 grid[i][j] 表示在位置 (i,j) 上累计的小正方体的个数，实际上就像搭积木一样，由这些小正方体来组成一个
     // 三维的物体，这里让我们求这个三维物体的表面积。我们知道每个小正方体的表面积是6，若在同一个位置累加两个，表面积就是10，三个累加到了一起
