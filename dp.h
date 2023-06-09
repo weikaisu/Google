157b5579c5c244deba1cc30817e86d3fc0a34875
SteveSu <weikai.su@gmail.com>
2023-06-09 17:00:05 +0800

    LC1013::canThreePartsEqualSum

--- dp.h
+++ dp.h
@@ -14,6 +14,7 @@ struct LC0070 { int climbStairs(int n); };
 struct LC0053 { int maxSubArray(vector<int>& nums); };
 
 //Greedy
+struct LC1013 { bool canThreePartsEqualSum(vector<int>& arr); };
 struct LC0717 { bool isOneBitCharacter(vector<int>& bits); };
 /*求兩兩一對較小數的和最大*/
 struct LC0561 { int arrayPairSum(vector<int>& nums); };
