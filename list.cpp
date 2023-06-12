c6a85d688f3bc1672f838554801cae02d9ef4672
SteveSu <weikai.su@gmail.com>
2023-06-12 19:31:13 +0800

    LC0118::generate

--- list.cpp
+++ list.cpp
@@ -1374,6 +1374,18 @@ vector<string> LC0228::summaryRanges(vector<int>& nums) {
     return res;
 }
 
+vector<vector<int>> LC0118::generate(int numRows) {
+    // 杨辉三角是二项式系数的一种写法，如果熟悉杨辉三角的五个性质，那么很好生成，可参见我的上一篇博文Pascal's Triangle II。具体生成算是：
+    // 每一行的首个和结尾一个数字都是1，从第三行开始，中间的每个数字都是上一行的左右两个数字之和。
+    vector<vector<int>> res(numRows, vector<int>());
+    for(int i=0; i<numRows; ++i) {
+        res[i].resize(i+1, 1);
+        for(int j=1; j<i; ++j)
+            res[i][j] = res[i-1][j] + res[i-1][j-1];
+    }
+    return res;
+}
+
 /***********  Linked List  **********/
 
 //LinkedList l2;
