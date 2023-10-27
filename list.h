73491908a6136292d41f17aa95faec837c4b8bb2
SteveSu <weikai.su@gmail.com>
2023-10-27 14:55:31 +0800

    LC1295::findNumbers

--- list.h
+++ list.h
@@ -130,6 +130,7 @@ struct LC0027 { int removeElement(vector<int>& nums, int val); };
 struct LC0026 { int removeDuplicates(vector<int>& nums); };
 
 /*********** Array List ***********/
+struct LC1295 { int findNumbers(vector<int>& nums); };
 struct LC1281 { int subtractProductAndSum(int n); };
 struct LC1266 { int minTimeToVisitAllPoints(vector<vector<int>>& points); };
 struct LC1252 { int oddCells(int m, int n, vector<vector<int>>& indices); };
