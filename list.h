171612f71258d3c08c6555c8f2e5193fd9a5a505
SteveSu <weikai.su@gmail.com>
2023-10-27 15:33:21 +0800

    vector<int> LC1304::sumZero

--- list.h
+++ list.h
@@ -130,6 +130,7 @@ struct LC0027 { int removeElement(vector<int>& nums, int val); };
 struct LC0026 { int removeDuplicates(vector<int>& nums); };
 
 /*********** Array List ***********/
+struct LC1304 { vector<int> sumZero(int n); };
 struct LC1299 { vector<int> replaceElements(vector<int>& arr);};
 struct LC1295 { int findNumbers(vector<int>& nums); };
 struct LC1281 { int subtractProductAndSum(int n); };
